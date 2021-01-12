5c49e330b8ed21b973e468cce67b35a983165b4d
Lyubov Burkova <lyubashaburkova@gmail.com>
2021-01-12 10:04:51 +0500

    if

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
-        float first, second, result;
+        int first, second;
+        boolean isTrue = false;
+        System.out.print("Enter");
+        first = num.nextInt();
 
-        System.out.print("Enter first num: ");
-        first = num.nextFloat();
+        isTrue = first == 51;
+        second = first == 15 ? 11 : 22;
 
-        System.out.print("Enter second num: ");
-        second = num.nextFloat();
+        System.out.println("isTrue "+ isTrue);
 
-        result = first + second;
-        System.out.print("Result = " + result);
+        if (first == 10) {
+            System.out.print("Num is 10");
+            System.out.print("\n");
+        }else{
+            System.out.print("Num is not 10");
+        }
+
+        switch (first){
+            case 51:
+                System.out.print("51!!!!");
+                break;
+            case 47:
+                System.out.print("47!!!!");
+                break;
+            case 30:
+                System.out.print("30!!!!");
+                break;
+            case 31:
+                System.out.print("31!!!!");
+                break;
+            default:
+                System.out.print("Num is equal something");
+        }
     }
 }
