aee2b4a73045903fadabd3367b7468293a461e7b
Lyubov Burkova <lyubashaburkova@gmail.com>
2021-01-12 10:30:25 +0500

    for, while, do

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,39 +3,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
-        int first, second;
-        boolean isTrue = false;
-        System.out.print("Enter");
-        first = num.nextInt();
-
-        isTrue = first == 51;
-        second = first == 15 ? 11 : 22;
-
-        System.out.println("isTrue "+ isTrue);
-
-        if (first == 10) {
-            System.out.print("Num is 10");
-            System.out.print("\n");
-        }else{
-            System.out.print("Num is not 10");
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
         }
 
-        switch (first){
-            case 51:
-                System.out.print("51!!!!");
-                break;
-            case 47:
-                System.out.print("47!!!!");
-                break;
-            case 30:
-                System.out.print("30!!!!");
-                break;
-            case 31:
-                System.out.print("31!!!!");
-                break;
-            default:
-                System.out.print("Num is equal something");
+        int j = 0;
+        boolean isTrue = true;
+        while (j < 10){
+            j++;
+            if (j % 2 == 0){
+                continue;
+            }
+            System.out.print("j =" + j);
         }
+
+        int k = 0;
+        do {
+            System.out.print("k = " + k);
+        }while (k<0);
     }
 }
